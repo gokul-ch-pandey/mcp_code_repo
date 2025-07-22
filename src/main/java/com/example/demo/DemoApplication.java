5ef359b44c3c21a5fe118548a438b7bd4377f4aa
Gokul Pandey <gokul.ch.pandey@gmail.com>
2025-07-22 11:34:40 +0530

    Fix: Update unit tests for LogGenerator and remove TODO

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -22,31 +22,16 @@ public class DemoApplication {
 		private final Logger logger = LoggerFactory.getLogger(LogGenerator.class);
 		private final Random random = new Random();
 		private final String[] messages = {
-			"Processing user request",
-			"Checking database connection",
-			"Validating input data",
-			"Performing background task",
-			"Cleaning up resources"
+				"Processing user request",
+				"Checking database connection",
+				"Validating input data",
+				"Performing background task",
+				"Cleaning up resources"
 		};
 
 		@Scheduled(fixedRate = 2000)
-		public void generateLogsAndExceptions() {
+		public void generateInfoLogs() { // Renamed method
 			logger.info(messages[random.nextInt(messages.length)]);
-
-			if (random.nextInt(100) < 30) {
-				String[] exceptions = {
-					"NullPointerException",
-					"IllegalArgumentException",
-					"RuntimeException",
-					"ArrayIndexOutOfBoundsException"
-				};
-				String exceptionType = exceptions[random.nextInt(exceptions.length)];
-				try {
-					throw new RuntimeException("Random " + exceptionType + " occurred!");
-				} catch (Exception e) {
-					logger.error("Exception occurred", e);
-				}
-			}
 		}
 	}
 }
